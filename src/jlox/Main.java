a6d6ec15c0da3e52482fb8be327d0c213f07182e
chhod-bhai <abigoku@gmail.com>
2025-09-25 01:55:51 +0530

    Left on page 45

---
+++ src/jlox/Main.java
@@ -0,0 +1,60 @@
+package jlox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Main {
+
+    static boolean hadError = false;
+
+    public static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+    }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.printf("[line %d] Error %s: %s", line, where, message);
+        hadError = true;
+    }
+
+    private static void runFile(String filePath) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(filePath));
+        run(new String(bytes, Charset.defaultCharset()));
+        if (hadError) System.exit(65);
+    }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader rdr = new BufferedReader(input);
+        for (; ; ) {
+            System.out.print("> ");
+            String line = rdr.readLine();
+            if (line == null) {
+                break;
+            }
+            run(line);
+            hadError = false;
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("USAGE: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+}
